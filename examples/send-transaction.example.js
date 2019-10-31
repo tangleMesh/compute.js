97952d7d5bc039cadb22fc92058cd55bc902b7fa
JeremiasEh <jeremias.ehnle@aol.com>
2019-10-31 16:35:25 +0100

    Add tanglemesh api-client.js + update example logs and values + update attachToTangle method to work properly and handle errors correctly

--- examples/send-transaction.example.js
+++ examples/send-transaction.example.js
@@ -7,7 +7,7 @@ const { composeAPI } = require ("@iota/core");
     // Set credentials for using the tangleMesh:api
     const apiIdentifier = "7DE635ECF529629BA113571D2A288598",
           apiSecret = "A4BEF4E7CE564EDD2BD8141F1D6546S567F659DC8F30D9918B2F17C387BD7B60",
-          channelId = "channel-id";
+          channelId = "7458d984-47ea-4ee1-bfbc-e3016a233f35";
     const apiAuth = new ApiAuthentificator (apiIdentifier, apiSecret);
 
     // Create the `compute` object for doing computation tasks
@@ -39,8 +39,8 @@ const { composeAPI } = require ("@iota/core");
             return compose.sendTrytes (trytes, 3, 14)
         })
         .then (bundle => {
-            console.log ("Explore your bundle:", "https://thetangle.org/transaction/" + bundle[0].hash);
             console.log ("The bundle:", bundle);
+            console.log ("Explore your bundle:", "https://thetangle.org/transaction/" + bundle[0].hash);
         })
         .catch (error => {
             console.error ("ERROR", error);
--- src/attatchToTangle.js
+++ src/attatchToTangle.js
@@ -2,28 +2,30 @@ module.exports = (channelId, apiClient) => {
     
     return async (trunkTransaction, branchTransaction, minWeightMagnitude, trytes, cb) => {
 
-        //If no callback defined, simply return the result itself
-        if (!cb) {
-            cb = (result) => result;
-        }
+        try {
+            //Create the request to tangleMesh:api
+            const result = await apiClient.post (
+                "/compute/attatchToTangle",
+                {
+                    channelId,
+                    trunkTransaction    : trunkTransaction,
+                    branchTransaction   : branchTransaction,
+                    minWeightMagnitude  : minWeightMagnitude,
+                    trytes              : trytes
+                },
+            );
 
-        //Create the request to tangleMesh:api
-        const result = await apiClient.post (
-            "/compute/attatchToTangle/" + encodeURIComponent (channelId),
-            {
-                trunkTransaction    : trunkTransaction,
-                branchTransaction   : branchTransaction,
-                minWeightMagnitude  : minWeightMagnitude,
-                trytes              : trytes
-            },
-        );
+            if (cb) {
+                return cb (result);
+            }
+            return result;
 
-        //Check for any error
-        if (result.status != 200) {
-            throw new Error (result.data);
+        } catch (e) {
+            if (cb) {
+                return cb (e);
+            }
+            throw e;
         }
-        //If result was successfull
-        return cb (result.trytes);
 
     };
 
